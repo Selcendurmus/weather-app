f928d64ba4be8aa4076887447194b806c4412afa
Selcendurmus <40351210+Selcendurmus@users.noreply.github.com>
2018-06-28 23:45:01 -0700

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { WeatherImageComponent } from './weather-image/weather-image.component';
 import { CityImageComponent } from './city-image/city-image.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { FiveDayWeatherForecastComponent } from './five-day-weather-forecast/five-day-weather-forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherImageComponent,
     CityImageComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    FiveDayWeatherForecastComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/five-day-weather-forecast/five-day-weather-forecast.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-five-day-weather-forecast',
+  template:`<p>
+  five-day-weather-forecast works!
+</p>
+`,
+  styles: ['']
+})
+export class FiveDayWeatherForecastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
