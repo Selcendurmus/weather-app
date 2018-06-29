a2bb4c43b146051cc0e626ab64c54bda22fa96c0
Selcendurmus <40351210+Selcendurmus@users.noreply.github.com>
2018-06-29 01:42:02 -0700

    interface-current-weather added

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from 'src/app/interfaces/icurrent-weather'
+
+
 
 @Component({
   selector: 'app-current-weather',
-  template:`<p>
-  current-weather works!
-</p>
-`,
-  styles: ['']
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
 
-  constructor() { }
+  constructor() { 
+    this.current = {
+    city: 'Seattle',
+    country: 'US',
+    date: new Date(),
+    image: '',
+    temperature: 72,
+    description: 'sunny', 
+  } as ICurrentWeather
+}
 
   ngOnInit() {
   }
