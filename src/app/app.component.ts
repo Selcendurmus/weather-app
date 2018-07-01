911725dc067bcc42873287d8087389db5bd28f0b
Selcendurmus <40351210+Selcendurmus@users.noreply.github.com>
2018-07-01 11:46:11 -0700

    updates on angular material and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,19 @@ import { Component } from '@angular/core';
   
   <div style="text-align:center">
   <mat-toolbar color="primary">
-    LocalCast Weather
+      LocalCast Weather
   </mat-toolbar>
-   <div>Your city, your forecast, right now!</div>
+   <div>
    <mat-card>
-    <h2>Current Weather</h2>
-    <app-current-weather></app-current-weather>
-    </mat-card>
-  </div>
+       <h3>Your city, your forecast, right now!</h3>
+      </mat-card> 
+      </div>
+             <mat-card>
+      <h2>Current Weather</h2>
+   <app-current-weather></app-current-weather>
+            </mat-card>
+            <img mat-card-image src="assets/seattlebg-day.jpg">
+              </div>
 `
 })
 export class AppComponent {
